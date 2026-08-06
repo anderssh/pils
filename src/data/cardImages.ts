8299dec0a1687357a9f1eb8c548da1d2311fde3d
Anders Stiksrud Helmen <anders.s.helmen@gmail.com>
2026-08-06 14:24:05 +0200

    optimization based on lighthous report

---
+++ src/data/cardImages.ts
@@ -0,0 +1,8 @@
+export const cardImages: Record<string, { src: string; width: number; height: number }> = {
+  "2018": { src: "images/cards/2018.webp", width: 415, height: 700 },
+  "2019": { src: "images/cards/2019.webp", width: 335, height: 700 },
+  "2020": { src: "images/cards/2020.webp", width: 335, height: 700 },
+  "2021": { src: "images/cards/2021.webp", width: 315, height: 700 },
+  "2023": { src: "images/cards/2023.webp", width: 315, height: 700 },
+  "2025": { src: "images/cards/2025.webp", width: 984, height: 700 },
+};
