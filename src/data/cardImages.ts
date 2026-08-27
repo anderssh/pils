d8d2eebd704b6fcbdae63b663ec6a84d658bf3a8
Anders Stiksrud Helmen <anders.s.helmen@gmail.com>
2026-08-27 10:22:49 +0200

    major cleanup, added new images from two tests

--- src/data/cardImages.ts
+++ src/data/cardImages.ts
@@ -1,7 +1,8 @@
 export const cardImages: Record<string, { src: string; width: number; height: number }> = {
-  "2018": { src: "images/cards/2018.webp", width: 415, height: 700 },
-  "2019": { src: "images/cards/2019.webp", width: 335, height: 700 },
-  "2020": { src: "images/cards/2020.webp", width: 335, height: 700 },
-  "2021": { src: "images/cards/2021.webp", width: 315, height: 700 },
-  "2025": { src: "images/cards/2025.webp", width: 984, height: 700 },
+  "2018": { src: "images/cards/pilotbryggeriet-lussekatt-trippel.webp", width: 415, height: 700 },
+  "2019": { src: "images/cards/nordlands-juleol.webp", width: 335, height: 700 },
+  "2020": { src: "images/cards/hot-rod-garage.webp", width: 335, height: 700 },
+  "2021": { src: "images/cards/disko-infernal.webp", width: 315, height: 700 },
+  "2023": { src: "images/2023/carlsberg-pilsner.webp", width: 714, height: 1000 },
+  "2025": { src: "images/cards/olympiske-leker-2025.webp", width: 984, height: 700 },
 };
